882c8ba39788d8935a4a908e9282debf64aabcb5
Shakeel Osmani <shakeel.osmani@gmail.com>
2023-09-12 01:42:47 -0400

    categorize them

--- src/components/todo/TodoView.js
+++ src/components/todo/TodoView.js
@@ -1,8 +1,38 @@
 import { TodoContext, useTodoContext } from "../../contexts/TodoContext";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const TodoView = () => {
   const { todos } = useTodoContext(TodoContext);
-  return <ol>{JSON.stringify(todos, "", 2)}</ol>;
+
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>Personal</Tab>
+        <Tab>Work</Tab>
+      </TabList>
+      <TabPanel>
+        <h5>Personal Todos:</h5>
+        <ol>
+          {todos
+            .filter((todo) => todo.todoCategory == "per")
+            .map((perTodo, i) => {
+              return <li key={i}> {perTodo.todoName}</li>;
+            })}
+        </ol>
+      </TabPanel>
+      <TabPanel>
+        <h5>Work Todos:</h5>
+        <ol>
+          {todos
+            .filter((todo) => todo.todoCategory == "work")
+            .map((perTodo, i) => {
+              return <li key={i}> {perTodo.todoName}</li>;
+            })}
+        </ol>
+      </TabPanel>
+    </Tabs>
+  );
 };
 
 export default TodoView;
